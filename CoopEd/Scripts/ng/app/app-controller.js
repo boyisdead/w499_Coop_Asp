5de6c203139fc9330187059686a74e7e886c5d77
boyisdead <nattawut_k@cmu.ac.th>
2015-08-24 00:25:46 +0700

    [zG-001]
     - reset bootstrap

--- CoopEd/Scripts/ng/app/app-controller.js
+++ CoopEd/Scripts/ng/app/app-controller.js
@@ -3,26 +3,7 @@
     function ($scope, $rootScope) {
         $rootScope.request = {};
 
-        $rootScope.api = api;
-
-        $rootScope.$on('cfpLoadingBar:started', function (event, data) {
-
-        })
-        $rootScope.$on('cfpLoadingBar:loading', function(event, data) {
-            $rootScope.request[data.request.method + ':' + data.request.url] = {
-                path: data.request.url,
-                status: 'pending',
-                method: data.request.method
-            };
-        });
-        $rootScope.$on('cfpLoadingBar:loaded', function(event, data) {
-            if (!!$rootScope.request[data.result.config.method + ':' + data.result.config.url]) {
-                $rootScope.request[data.result.config.method + ':' + data.result.config.url].status = (data.result.status >= 200 && data.result.status < 300 ? 'success' : 'error');
-            }
-        });
-        $rootScope.$on('cfpLoadingBar:completed', function(event, data) {
-            $rootScope.request = {};
-        });
+        //$rootScope.api = api;
 
         $scope.testtest = "Hello";
 
